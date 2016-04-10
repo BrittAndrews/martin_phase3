e30b5691edb9b3f0a4ababcdeffd452130ea81fe
BRITTANY ANDREWS <brittanyandrewsnyc@gmail.com>
2016-04-10 15:24:42 -0400

    slideshow working, collaspable nav bar too, tryign to get about html to link but no luck

--- src/js/index.js
+++ src/js/index.js
@@ -2,17 +2,32 @@ import $ from "jquery";
 
 
 // slideshow/////////
+// original slide show not working
+// $("#slideshow > div:gt(0)").hide();
 
-$("#slideshow > div:gt(0)").hide();
-
-setInterval(function() { 
-  $('#slideshow > div:first')
-    .fadeOut(5000)
-    .next()
-    .fadeIn(5000)
-    .end()
-    .appendTo('#slideshow');
-},  8000);
+// setInterval(function() { 
+//   $('#slideshow > div:first')
+//     .fadeOut(5000)
+//     .next()
+//     .fadeIn(5000)
+//     .end()
+//     .appendTo('#slideshow');
+// },  8000);
 
 
 
+
+
+//SLIDESHOW////////////////////
+
+$('.img-wrapper img:gt(0)').hide();
+
+$('.next').click(function() {
+    $('.img-wrapper img:first-child').fadeOut().next().fadeIn().end().appendTo('.img-wrapper');
+});
+
+$('.prev').click(function() {
+    $('.img-wrapper img:first-child').fadeOut();
+    $('.img-wrapper img:last-child').prependTo('.img-wrapper').fadeOut();
+    $('.img-wrapper img:first-child').fadeIn();
+});
